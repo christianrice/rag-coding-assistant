b9f8801eb72607be878162496d4ae1910d383fc8
christianrice <christian.e.rice@gmail.com>
2024-01-29 09:39:14 -0600

    Added demos for LangGraph agents

--- data/example_8.py
+++ data/example_8.py
@@ -2,12 +2,13 @@
 tags: [langchain]
 description: |
     - Accept string
-    - Format single prompt with output instructions using Pydantic
+    - Format single prompt with format instructions using Pydantic
     - Parse response using Pydantic
 """
 # Create a chain that does the following:
-# - Accept string as input
-# - Format the prompt using variables from the object. The prompt has output instructions using Pydantic
+# - Accept string named query as input
+# - Structure the input as an object to pass to the prompt
+# - Format the prompt using variables from the object. The prompt includes format instructions using Pydantic
 # - Pass the prompt to OpenAI
 # - Parse the response using Pydantic
 from typing import List
@@ -18,9 +19,11 @@ from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain_core.runnables import RunnablePassthrough
 
+
 class Actor(BaseModel):
     name: str = Field(description="name of an actor")
     film_names: List[str] = Field(description="list of names of films they starred in")
+
 
 parser = PydanticOutputParser(pydantic_object=Actor)
 
@@ -38,11 +41,6 @@ prompt = PromptTemplate(
 
 model = ChatOpenAI()
 
-chain = (
-    {"query": RunnablePassthrough()}
-    | prompt
-    | model
-    | parser
-)
+chain = {"query": RunnablePassthrough()} | prompt | model | parser
 
 chain.invoke("Generate the filmography for a random actor from Birdman.")
